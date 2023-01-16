348971bdcc113ca3ae1db202e142d414b4de7daf
Hlayan Htet Aung <hlayanhtetaung@gmail.com>
2023-01-17 00:48:34 +0630

    Upgrade gradle and related dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,37 +6,30 @@ plugins {
     id("dagger.hilt.android.plugin")
 }
 
-val env: MutableMap<String, String> = System.getenv()
-
 android {
-    compileSdk = 32
+    compileSdk = 33
     namespace = "com.hlayan.scripty"
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 2
         versionName = "1.0.1"
         applicationId = "com.hlayan.scripty"
         vectorDrawables.useSupportLibrary = true
     }
 
-    val releaseSigningConfig = signingConfigs.create("release") {
-        storeFile = File(env["ReleaseStoreFile"]!!)
-        storePassword = env["ReleaseStorePassword"]
-        keyAlias = env["ReleaseKeyAlias"]
-        keyPassword = env["ReleaseKeyPassword"]
-    }
-
     buildTypes {
-        getByName("debug") {
+        debug {
             versionNameSuffix = "-debug"
             applicationIdSuffix = ".debug"
         }
-        getByName("release") {
+        release {
             isMinifyEnabled = true
             isShrinkResources = true
-            signingConfig = releaseSigningConfig
+
+            // Debug key signing is available to everyone.
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
@@ -51,25 +44,24 @@ android {
     }
 
     composeOptions {
-        useLiveLiterals = false
-        kotlinCompilerExtensionVersion = "1.1.1"
+        kotlinCompilerExtensionVersion = "1.3.2" // needed kotlin version 1.7.20
     }
 }
 
 dependencies {
 
-    val composeVersion = "1.1.1"
+    implementation(platform("androidx.compose:compose-bom:2023.01.00"))
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0")
-    implementation("androidx.activity:activity-compose:1.5.0")
-    implementation("androidx.core:core-splashscreen:1.0.0-rc01")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.core:core-splashscreen:1.0.0")
 
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("com.google.android.material:material:1.7.0")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.17.0")
 
     implementation("com.jakewharton.timber:timber:5.0.1")
@@ -79,9 +71,9 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     kapt("com.google.dagger:hilt-android-compiler:2.38.1")
 
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.0")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
 
 // Allow references to generated code
